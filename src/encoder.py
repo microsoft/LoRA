a7765e32a2a85b20fa87348d6027ca165d2bca5a
wallisp <wallis.phillip@gmail.com>
2021-06-18 10:32:29 -0700

    adding cr info

--- src/encoder.py
+++ src/encoder.py
@@ -1,9 +1,7 @@
-"""Byte pair encoding utilities"""
-
-
-
-
-
+#  ------------------------------------------------------------------------------------------
+#  Copyright (c) Microsoft Corporation. All rights reserved.
+#  Licensed under the MIT License (MIT). See LICENSE in the repo root for license information.
+#  ------------------------------------------------------------------------------------------
 import os
 import json
 import regex as re
