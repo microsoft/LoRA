13b369026a44bff13a881b882a558fe62487b74a
phwallis <46939417+phwallis@users.noreply.github.com>
2021-06-19 19:51:04 -0700

    Update download_evalscript.sh

--- eval/download_evalscript.sh
+++ eval/download_evalscript.sh
@@ -4,6 +4,7 @@ cd eval
 echo "installing evaluation dependencies"
 echo "downloading e2e-metrics..."
 git clone https://github.com/tuetschek/e2e-metrics e2e
+pip install -r e2e/requirements.txt
 
 echo "downloading GenerationEval for webnlg and dart..."
 git clone https://github.com/WebNLG/GenerationEval.git
