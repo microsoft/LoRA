815eefab950a13d8850a8e82b911d090a3859d2f
wallisp <wallis.phillip@gmail.com>
2021-07-11 13:34:11 -0700

    formatting 2-space tabs to 4-space

--- src/gpt2_encode.py
+++ src/gpt2_encode.py
@@ -37,34 +37,34 @@ parser.add_argument('--vocab', type=str, default=None, help='vocab path')
 parser.add_argument('--output', default=None, type=str, help='ft output file')
 parser.add_argument('--add_bos', action='store_true', help='')
 parser.add_argument('--add_eos', action='store_true', help='')
-
 args = parser.parse_args()
 
+
 if __name__ == "__main__":
-  enc = encoder.get_encoder(args.vocab)
-  
-  writer = open(args.output, 'w')
+    enc = encoder.get_encoder(args.vocab)
+    
+    writer = open(args.output, 'w')
 
-  with open(args.input, 'r') as reader:
-    line_idx = 0
-    for line in reader:
-      items = json.loads(line.strip())
-      context = items['context']
-      completion = items['completion']
+    with open(args.input, 'r') as reader:
+        line_idx = 0
+        for line in reader:
+            items = json.loads(line.strip())
+            context = items['context']
+            completion = items['completion']
 
-      bos = 50256
-      eos = 50256
-      context_bpes, _ = enc.encode(context) 
-      context_bpes += [bos] if args.add_bos else []
+            bos = 50256
+            eos = 50256
+            context_bpes, _ = enc.encode(context) 
+            context_bpes += [bos] if args.add_bos else []
 
-      completion_bpes, _ = enc.encode(' ' + completion)
-      completion_bpes += [eos] if args.add_eos else []
+            completion_bpes, _ = enc.encode(' ' + completion)
+            completion_bpes += [eos] if args.add_eos else []
 
-      ft_json = {}
-      ft_json['context'] = context_bpes
-      ft_json['completion'] = completion_bpes 
-      writer.write(json.dumps(ft_json)+'\n')
+            ft_json = {}
+            ft_json['context'] = context_bpes
+            ft_json['completion'] = completion_bpes 
+            writer.write(json.dumps(ft_json)+'\n')
 
-      line_idx += 1
+            line_idx += 1
 
-  writer.close()
+    writer.close()
